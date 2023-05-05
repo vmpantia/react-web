dabad937b4998df11e1016023c08aa5a8090d8d7
Vincent Pantia <vincent.m.pantia@hotmail.com>
2023-05-05 12:18:27 +0800

    [u] Changes

---
+++ src/component/SideBar/SideBar.tsx
@@ -0,0 +1,19 @@
+const SideBar  = () => {
+    return (
+        <div className="sidebar">
+            <ul>
+                <li>
+                    <a>Employee</a>
+                </li>
+                <li>
+                    <a>Department</a>
+                </li>
+                <li>
+                    <a>Position</a>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default SideBar;
